07f9746977a395e6ff3d6d7f168fcd08986a29d2
Simofor <81316675+Simofor@users.noreply.github.com>
2022-05-04 19:06:34 +0300

    git add

--- Yalfer 2.7/config/config.py
+++ Yalfer 2.7/config/config.py
@@ -1,12 +1,11 @@
 EMBED_COLOR = 0x74d145
-EMBED_COLOR_ERROR = 0xE60042
-EMBED_COLOR_LOG = 0xD60062
-VERSION = 2.6
+EMBED_COLOR_ERROR = 0xD60062
+VERSION = 2.7
 DEVELOPER = "Diamond#7941 || aleleo#8899"
 NAME = "Yalfer"
-TEACHER = 10000
-PILOT = 25000
-MEDIC = 50000
+WORKER = 50000
+PILOT = 100000
+MEDIC = 150000
 JOIN_MSG = ":wave: На связи Yalfer - индивидуальный помощник в сфере Discord!\
     \nК Вашим услугам:\
     \n - Майнинг\
